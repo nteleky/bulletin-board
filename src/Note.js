b4c49bd24543a4e553dfe4c0d3c3d7fd11bf5c70
Nick Teleky <nteleky@gmail.com>
2018-05-10 11:49:26 +0200

    Set up state to edit notes

--- src/Note.js
+++ src/Note.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react';
 import FaPencil from 'react-icons/lib/fa/pencil';
 import FaTrash from 'react-icons/lib/fa/trash';
+import FaFloppyO from 'react-icons/lib/fa/floppy-o';
+
 
 class Note extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            editing: false
+        };
+        this._newText = "yada"
         this.edit = this.edit.bind(this);
-        this.remove = this.edit.bind(this);
+        this.remove = this.remove.bind(this);
+        this.save = this.save.bind(this);
+        this.renderForm = this.renderForm.bind(this);
+        this.renderDisplay = this.renderDisplay.bind(this);
     }
 
     edit() {
-        console.log('editing note!');
+        this.setState({
+            editing: true
+        });
+    }
+
+    save(e) {
+        e.preventDefault();
+        console.log(this._newText.value);
+        this.setState({
+            editing: false
+        });
     }
 
     remove() {
         console.log('removing note!');
     }
 
+    renderForm() {
+        return (
+            <form>
+                <textarea ref={input => this._newText = input} />
+                <button onClick={this.save}><FaFloppyO /></button>
+            </form>
+        )
+    }
+
+    renderDisplay() {
+        return (
+            <p> {this._newText.value} </p>
+        )
+    }
+
     render() {
         return (
             <div className="note">
-                <p>Learn React</p>
+                {this.state.editing ? this.renderForm() : this.renderDisplay()}
                 <span>
                     <button onClick={this.edit} id="edit"><FaPencil /></button>
                     <button onClick={this.remove} id="remove"><FaTrash /></button>
